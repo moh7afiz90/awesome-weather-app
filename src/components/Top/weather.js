e8236ffe0c3fd28509131584a530c01bfff8e576
Mohanad Elhag <moh.7afiz@gmail.com>
2019-05-19 06:22:38 +0300

    style Top section

--- src/components/Top/weather.js
+++ src/components/Top/weather.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 export default class Weather extends React.Component {
+  state = {};
   render() {
     return (
       <div className="weather-container">
-        <div className="header" />
+        <div className="header">Location Name</div>
         <div className="inner-container">
           <div className="image">IMG here</div>
           <div className="current-weather">10</div>
